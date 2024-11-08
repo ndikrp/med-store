4f58bc733b285dbd62ab44a74e4204b471227cb5
ndikrp <andhikarizky36@gmail.com>
2024-11-08 20:35:13 +0700

    add: cart management

---
+++ app/Livewire/ProductDetailPage.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Models\Product;
+use Livewire\Component;
+use Livewire\Attributes\Title;
+
+#[Title('Product Detail - SISEHAT')]
+class ProductDetailPage extends Component
+{
+
+    public $slug;
+
+    public function mount($slug)
+    {
+        $this->slug = $slug;
+    }
+    public function render()
+    {
+        return view('livewire.product-detail-page', [
+            'product' => Product::where('slug', $this->slug)->firstOrFail()
+        ]);
+    }
+}
---
+++ app/Livewire/ProductsPage.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Models\Brand;
+use App\Models\Category;
+use App\Models\Product;
+use Livewire\Component;
+use Livewire\Attributes\Title;
+use Livewire\WithPagination;
+
+#[Title('Products - SISEHAT')]
+
+class ProductsPage extends Component
+{
+    use WithPagination;
+    public function render()
+    {
+        $productQuery = Product::query()->where('is_active', 1);
+        return view('livewire.products-page', [
+            'products' => $productQuery->paginate(6),
+            'brands' => Brand::where('is_active', 1)->get(['id', 'name', 'slug']),
+            'categories' => Category::where('is_active', 1)->get(['id', 'name', 'slug'])
+        ]);
+    }
+}
